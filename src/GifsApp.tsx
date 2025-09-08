c045f6c07c4999b43bc7b666e29c960a96927440
Diego Caucota <diegocaucota@gmail.com>
2025-09-08 12:06:34 -0300

    fix: GifsApp axios

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -2,14 +2,17 @@ import { useState } from 'react';
 
 import { PreviousSearches } from './gifs/components/PreviousSearches';
 import { GifList } from './gifs/components/GifList';
-import { mockGifs } from './mock-data/gifs.mock';
 import { SearchBar } from './shared/components/SearchBar';
 import { CustomHeader } from './shared/components/CustomHeader';
 
 import { getGifsByQuery } from './gifs/actions/get-gifs-by-query.action';
+import type { Gif } from './gifs/interfaces/gif.interface';
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+
+  const [gifs, setGifs] = useState<Gif[]>([])
+
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
   const handleTermClicked = (term: string) => {
     console.log(term);
@@ -31,7 +34,7 @@ export const GifsApp = () => {
     // Aqui llamo la query a la API
     const gifs = await getGifsByQuery(query);
 
-    console.log(gifs);
+    setGifs(gifs)
   };
 
   return (
@@ -57,8 +60,7 @@ export const GifsApp = () => {
       />
 
       {/* Gifs */}
-      {/* "gifs" es la property y "mockGifs" son los datos enviados a través de esa props */}
-      <GifList gifs={mockGifs} />
+      <GifList gifs={gifs} />
     </>
   );
 };
--- src/gifs/actions/get-gifs-by-query.action.ts
+++ src/gifs/actions/get-gifs-by-query.action.ts
@@ -1,26 +1,26 @@
-import axios from "axios";
 import type { GhipyResponse } from "../interfaces/giphy.response";
 import type { Gif } from "../interfaces/gif.interface";
+import { giphyApi } from "../api/giphy.api";
 
+// Importo la instancia que cree de axios y agrego parametros para esta query en particular 
 export const getGifsByQuery = async (query: string): Promise<Gif[]> => {
-
-    const response = await axios.get<GhipyResponse>('https://api.giphy.com/v1/gifs/search', {
-        params: {
-            q: query,
-            limit: 10,
-            lang: 'es',
-            api_key: 'B6ZR4HBAkoZQ3bSUe5lLwz9iJXeDt5Mx'
+    const response = await giphyApi<GhipyResponse>('/search',
+        {
+            params: {
+                q: query,
+                limit: 10,
+            },
         }
-    })
+    );
 
-    // Aqui realizo mi transformación a mi interface "gif"
+    // Aqui transformo la respuesta de axios a un interface "gif"
     return response.data.data.map((gif) => ({
         id: gif.id,
         title: gif.title,
         url: gif.images.original.url,
         width: Number(gif.images.original.width),
-        height: Number(gif.images.original.height)
-    }))
+        height: Number(gif.images.original.height),
+    }));
 
     //     fetch(
     //     `https://api.giphy.com/v1/gifs/search?api_key=B6ZR4HBAkoZQ3bSUe5lLwz9iJXeDt5Mx&q=${query}&limit=10&lang=es`
